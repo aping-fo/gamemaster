26ce563b03b3ee8f5ebc953bda6e883932ce3883
Aping-Fo <125455364@qq.com>
2018-03-28 15:38:06 +0800

    增加充值记录查询

--- api/src/main/java/com/luckygames/wmxz/gamemaster/controller/RechargeController.java
+++ api/src/main/java/com/luckygames/wmxz/gamemaster/controller/RechargeController.java
@@ -2,9 +2,12 @@ package com.luckygames.wmxz.gamemaster.controller;
 
 import com.github.pagehelper.Page;
 import com.luckygames.wmxz.gamemaster.controller.base.BaseController;
+import com.luckygames.wmxz.gamemaster.model.entity.PlayerOrder;
 import com.luckygames.wmxz.gamemaster.model.entity.RechargeDaily;
 import com.luckygames.wmxz.gamemaster.model.view.base.Response;
+import com.luckygames.wmxz.gamemaster.model.view.request.PlayerOrderSearchQuery;
 import com.luckygames.wmxz.gamemaster.model.view.request.RechargeDailySearchQuery;
+import com.luckygames.wmxz.gamemaster.service.PlayerOrderService;
 import com.luckygames.wmxz.gamemaster.service.RechargeDailyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +20,8 @@ public class RechargeController extends BaseController {
 
     @Autowired
     private RechargeDailyService rechargeDailyService;
+    @Autowired
+    private PlayerOrderService playerOrderService;
 
     @RequestMapping(value = "/daily", method = {RequestMethod.GET, RequestMethod.POST})
     public Response dailyRecharge(RechargeDailySearchQuery rechargeDailySearchRequest) {
@@ -25,9 +30,22 @@ public class RechargeController extends BaseController {
         }
         Page<RechargeDaily> rechargeDailies = rechargeDailyService.searchPage(rechargeDailySearchRequest);
 
-        Response r = new Response("recharge/daily")
+        Response r = new Response("/recharge/daily")
                 .request(rechargeDailySearchRequest)
                 .data("rechargeDailyList", rechargeDailies);
+        return r;
+    }
+
+    @RequestMapping(value = "/order", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response queryOrderList(PlayerOrderSearchQuery playerOrderSearchQuery) {
+        if (playerOrderSearchQuery.getPageNum() == null) {
+            playerOrderSearchQuery.setPageNum(1);
+        }
+        Page<PlayerOrder> orderDailies = playerOrderService.searchPage(playerOrderSearchQuery);
+
+        Response r = new Response("/recharge/order")
+                .request(playerOrderSearchQuery)
+                .data("orderList", orderDailies);
         return r;
     }
 }
--- api/src/main/java/com/luckygames/wmxz/gamemaster/service/PlayerOrderServiceImpl.java
+++ api/src/main/java/com/luckygames/wmxz/gamemaster/service/PlayerOrderServiceImpl.java
@@ -1,7 +1,49 @@
 package com.luckygames.wmxz.gamemaster.service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.luckygames.wmxz.gamemaster.dao.PlayerOrderExample;
+import com.luckygames.wmxz.gamemaster.dao.mapper.PlayerOrderMapper;
+import com.luckygames.wmxz.gamemaster.model.entity.PlayerOrder;
+import com.luckygames.wmxz.gamemaster.model.view.request.PlayerOrderSearchQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service("playerOrderService")
 public class PlayerOrderServiceImpl implements PlayerOrderService {
+    @Autowired
+    private PlayerOrderMapper playerOrderMapper;
+    public static final String FORMAT_STR = "yyyy-MM-dd HH:mm:ss";
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMAT_STR);
+    private static final ZoneId zoneId = ZoneId.systemDefault();
+
+    @Override
+    public Page<PlayerOrder> searchPage(PlayerOrderSearchQuery query) {
+        PlayerOrderExample example = new PlayerOrderExample();
+        PlayerOrderExample.Criteria criteria = example.createCriteria();
+
+        if (query.getServerId() != null && !query.getServerId().isEmpty()) {
+            criteria.andServerIdIn(query.getServerId());
+        }
+        if (query.getChannelId() != null && !query.getChannelId().isEmpty()) {
+            criteria.andChannelIdIn(query.getChannelId());
+        }
+
+        if (!StringUtils.isEmpty(query.getStartDate()) && !StringUtils.isEmpty(query.getEndDate())) {
+            LocalDateTime startLocalDateTime = LocalDateTime.parse(query.getStartDate(), formatter);
+            LocalDateTime endLocalDateTime = LocalDateTime.parse(query.getEndDate(), formatter);
+            ZonedDateTime zdtStart = startLocalDateTime.atZone(zoneId);
+            ZonedDateTime zdtEnd = endLocalDateTime.atZone(zoneId);
+            criteria.andRechargedDateBetween(Date.from(zdtStart.toInstant()), Date.from(zdtEnd.toInstant()));
+        }
+
+        return PageHelper.startPage(query.getPageNum(), query.getPageSize()).doSelectPage(() -> playerOrderMapper.selectByExample(example));
+    }
 }
