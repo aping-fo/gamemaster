61d319f1685df1ed36d0e42b7bc9a2552ec28260
aping-fo <125455364@qq.com>
2018-05-22 16:25:25 +0800

    增加服务器ID参数

--- api/src/main/java/com/luckygames/wmxz/gamemaster/model/view/base/GMQuery.java
+++ api/src/main/java/com/luckygames/wmxz/gamemaster/model/view/base/GMQuery.java
@@ -1,11 +1,22 @@
 package com.luckygames.wmxz.gamemaster.model.view.base;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 /**
  * Created by lucky on 2018/3/29.
  */
 public class GMQuery {
+    private List<Long> serverId;
+
+    public List<Long> getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(List<Long> serverId) {
+        this.serverId = serverId;
+    }
+
     public String encodeReqParams() {
         StringBuilder sb = new StringBuilder("?");
         Field[] fields = this.getClass().getDeclaredFields();
