b727d42685eb979e6642123c806b268ce00bd872
plxz <453578151@qq.com>
2018-06-13 20:35:09 +0800

    活动

--- api/src/main/java/com/luckygames/wmxz/gamemaster/controller/GameController.java
+++ api/src/main/java/com/luckygames/wmxz/gamemaster/controller/GameController.java
@@ -4,14 +4,8 @@ import com.github.pagehelper.Page;
 import com.luckygames.wmxz.gamemaster.controller.base.BaseController;
 import com.luckygames.wmxz.gamemaster.model.entity.*;
 import com.luckygames.wmxz.gamemaster.model.view.base.Response;
-import com.luckygames.wmxz.gamemaster.model.view.request.ChannelSearchQuery;
-import com.luckygames.wmxz.gamemaster.model.view.request.GiftpackageAddSearchQuery;
-import com.luckygames.wmxz.gamemaster.model.view.request.GiftpackageSyncSearchQuery;
-import com.luckygames.wmxz.gamemaster.model.view.request.ServerSearchQuery;
-import com.luckygames.wmxz.gamemaster.service.ChannelService;
-import com.luckygames.wmxz.gamemaster.service.GiftpackageAddService;
-import com.luckygames.wmxz.gamemaster.service.GiftpackageSyncService;
-import com.luckygames.wmxz.gamemaster.service.ServerService;
+import com.luckygames.wmxz.gamemaster.model.view.request.*;
+import com.luckygames.wmxz.gamemaster.service.*;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -32,6 +26,8 @@ public class GameController extends BaseController {
     private ServerService serverService;
     @Autowired
     private ChannelService channelService;
+    @Autowired
+    private ActivityService activityService;
 
     //礼包管理
     @RequestMapping(value = "/giftpackage", method = {RequestMethod.GET, RequestMethod.POST})
@@ -80,5 +76,49 @@ public class GameController extends BaseController {
                 .data("serverList", serverList)
                 .data("channelList", channelList)
                 .data("giftPackageList", giftPackagePage);
+    }
+
+    //活动
+    @RequestMapping(value = "/event", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response activity() {
+        return new Response("activity/activity");
+    }
+
+    //奥林匹克活动列表
+    @RequestMapping(value = "/olympics_activity_list", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response activityList(ActivitySearchQuery query) {
+        if(query.getId()!=null){
+            activityService.update(query);
+        }
+        Page<Activity> activityPage = activityService.searchPage(query);
+        return new Response("activity/olympics_activity_list")
+                .request(query)
+                .data("activityList",activityPage);
+    }
+
+    //奥林匹克活动设置
+    @RequestMapping(value = "/olympics_activity_setting", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response activitySetting(Activity activity) {
+        if(StringUtils.isNotBlank(activity.getTitle())){
+            activityService.add(activity);
+        }
+        return new Response("activity/olympics_activity_setting");
+    }
+
+    //奥林匹克单服活动
+    @RequestMapping(value = "/olympics_single_activity", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response activitySingleActivity(CommonSearchQuery query) {
+        return new Response("activity/olympics_single_activity")
+                .request(query);
+    }
+
+    //奥林匹克活动审核
+    @RequestMapping(value = "/olympics_activity_audit", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response activityAudit(ActivitySearchQuery query) {
+        query.setActivityStatus(0);
+        Page<Activity> activityPage = activityService.searchPage(query);
+        return new Response("activity/olympics_activity_audit")
+                .request(query)
+                .data("activityList",activityPage);
     }
 }
